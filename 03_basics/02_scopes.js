be15e0dbc84d1ae6282dd5ea1e876d385fefa7d6
thufit23 <152521998+thufit23@users.noreply.github.com>
2023-12-03 08:09:34 +0000

    scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,10 @@
+function one() {
+    const username = "mayank"
+    function two() {
+        const wesbite = "poohub"
+        console.log(username)
+    }
+    console.log(website)
+    two()
+}
+one()
